d4c6468ba84b03f73c2a9f8432fabd925ee8554c
kanechan25 <kanechan.civilengineer@gmail.com>
2024-01-22 15:18:24 +0700

    Rebuild

---
+++ docs/_nuxt/14fd3a6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{213:function(n,t,o){"use strict";o.r(t);var e=o(5),component=Object(e.a)({},(function(){var n=this._self._c;return n("div",[n("nuxt-link",{attrs:{to:"/"}},[this._v("Go back Home")])],1)}),[],!1,null,null,null);t.default=component.exports}}]);
